d40d37fd2921027ff044f888d9b3923e4669ff1c
ballsai <jarunemail@gmail.com>
2017-10-16 18:31:31 +0700

    Change image when character move

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ PLAYER_MARGIN = 20
 BOX_MARGIN = 40
 MOVEMENT_SPEED = 2
 
+
 class Model:
     def __init__(self, world, x, y):
         self.world = world
@@ -19,33 +20,99 @@ class Model:
                   self.x<= obj.x+obj_right  )
 
 
-class Player(Model):
+class Player(arcade.Sprite):
 
-    def __init__(self, world, x,y):
-        super().__init__(world, x, y)
+    def __init__(self):
+        super().__init__()
 
-    def control(self, keys):
-        if keys[key.UP]:
-            self.y += MOVEMENT_SPEED
-        elif keys[key.DOWN]:
-            self.y -= MOVEMENT_SPEED
-        elif keys[key.LEFT]:
-            self.x -= MOVEMENT_SPEED
-        elif keys[key.RIGHT]:
-            self.x += MOVEMENT_SPEED
+
+        self.texture_left = []
+        self.texture_left.append(arcade.load_texture("images/player/char_w01.png",scale =2))
+        self.texture_left.append(arcade.load_texture("images/player/char_w02.png",scale =2))
+        self.texture_left.append(arcade.load_texture("images/player/char_w03.png",scale =2))
+        self.texture_left.append(arcade.load_texture("images/player/char_w04.png",scale =2))
+        self.texture_left.append(arcade.load_texture("images/player/char_w05.png",scale =2))
+        self.texture_left.append(arcade.load_texture("images/player/char_w06.png",scale =2))
+        self.texture_left.append(arcade.load_texture("images/player/char_w07.png",scale =2))
+        self.texture_left.append(arcade.load_texture("images/player/char_w08.png",scale =2))
+        self.texture_left.append(arcade.load_texture("images/player/char_w09.png",scale =2))
+
+        self.texture_right = []
+        self.texture_right.append(arcade.load_texture("images/player/char_e01.png",scale =2))
+        self.texture_right.append(arcade.load_texture("images/player/char_e02.png",scale =2))
+        self.texture_right.append(arcade.load_texture("images/player/char_e03.png",scale =2))
+        self.texture_right.append(arcade.load_texture("images/player/char_e04.png",scale =2))
+        self.texture_right.append(arcade.load_texture("images/player/char_e05.png",scale =2))
+        self.texture_right.append(arcade.load_texture("images/player/char_e06.png",scale =2))
+        self.texture_right.append(arcade.load_texture("images/player/char_e07.png",scale =2))
+        self.texture_right.append(arcade.load_texture("images/player/char_e08.png",scale =2))
+        self.texture_right.append(arcade.load_texture("images/player/char_e09.png",scale =2))
+
+        self.texture_down = []
+        self.texture_down.append(arcade.load_texture("images/player/char_s01.png",scale =2))
+        self.texture_down.append(arcade.load_texture("images/player/char_s02.png",scale =2))
+        self.texture_down.append(arcade.load_texture("images/player/char_s03.png",scale =2))
+        self.texture_down.append(arcade.load_texture("images/player/char_s04.png",scale =2))
+        self.texture_down.append(arcade.load_texture("images/player/char_s05.png",scale =2))
+        self.texture_down.append(arcade.load_texture("images/player/char_s06.png",scale =2))
+        self.texture_down.append(arcade.load_texture("images/player/char_s07.png",scale =2))
+        self.texture_down.append(arcade.load_texture("images/player/char_s08.png",scale =2))
+        self.texture_down.append(arcade.load_texture("images/player/char_s09.png",scale =2))
+
+        self.texture_up = []
+        self.texture_up.append(arcade.load_texture("images/player/char_n01.png",scale =2))
+        self.texture_up.append(arcade.load_texture("images/player/char_n02.png",scale =2))
+        self.texture_up.append(arcade.load_texture("images/player/char_n03.png",scale =2))
+        self.texture_up.append(arcade.load_texture("images/player/char_n04.png",scale =2))
+        self.texture_up.append(arcade.load_texture("images/player/char_n05.png",scale =2))
+        self.texture_up.append(arcade.load_texture("images/player/char_n06.png",scale =2))
+        self.texture_up.append(arcade.load_texture("images/player/char_n07.png",scale =2))
+        self.texture_up.append(arcade.load_texture("images/player/char_n08.png",scale =2))
+        self.texture_up.append(arcade.load_texture("images/player/char_n09.png",scale =2))
+        
+        self.index = 0
+        self.texture = self.texture_down[self.index]
     
 
-    def update(self, delta):
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
         
-        if self.x < PLAYER_MARGIN:
-            self.x += MOVEMENT_SPEED
-        elif self.x > self.world.width-PLAYER_MARGIN:
-            self.x -= MOVEMENT_SPEED
-            
-        elif self.y<PLAYER_MARGIN+30:
-            self.y += MOVEMENT_SPEED 
-        elif self.y >self.world.height-PLAYER_MARGIN-30:
-            self.y -= MOVEMENT_SPEED 
+       
+        if self.change_x < 0:
+            self.index += 1
+            if self.index >= len(self.texture_left):
+                self.index = 0
+            self.texture = self.texture_left[self.index]
+
+        elif self.change_x > 0:
+            self.index += 1
+            if self.index >= len(self.texture_right):
+                self.index = 0
+            self.texture = self.texture_right[self.index]
+
+        elif self.change_y <0:
+            self.index += 1
+            if self.index >= len(self.texture_down):
+                self.index = 0
+            self.texture = self.texture_down[self.index]
+       
+        elif self.change_y >0:
+            self.index += 1
+            if self.index >= len(self.texture_up):
+                self.index = 0
+            self.texture = self.texture_up[self.index]
+
+        if self.left < 0:
+            self.left = 0
+        elif self.right > 800 - 1:
+            self.right = 600 - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > 800 - 1:
+            self.top = 600 - 1
+
 
 
 class Box(Model):
@@ -63,48 +130,55 @@ class Box(Model):
             self.y -= MOVEMENT_SPEED 
 
 class Ghost_LV1(Model):
+    
+   
     def __init__(self, world, x, y):
         super().__init__(world, x, y)
+        self.count_time = 0
 
     def update(self, delta):
+        
+    
+        pass
 
-        if abs(self.x-self.world.player.x) >= 1:
-            
-            if self.x < self.world.player.x:
-                self.x += 1
-            if self.x > self.world.player.x:
-                self.x -= 1
-        if abs(self.y-self.world.player.y) >= 1:
-            if self.y < self.world.player.y:
-                self.y += 1
-            if self.y > self.world.player.y:
-                self.y -= 1
-          
-
-
+      
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
         
-        self.player = Player(self, 100,200)
+        self.player = Player()
+        self.player.center_x = self.width//2
+        self.player.center_y = self.height//2
+
         self.box = Box(self, 300,400)
         self.ghost_lv1 = Ghost_LV1(self,50,50)
 
 
-    def control(self, keys):
-        self.player.control(keys)
-        if keys[key.SPACE]:
-           print("SPACE")
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP:
+            self.player.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player.change_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+       
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player.change_x = 0
         
-
-
+        
     def update(self, delta):
-        self.player.update(delta)
+        self.player.update()
         self.ghost_lv1.update(delta)
         
         
-        if self.player.hit_obj(self.box,0,40,-60,60):
+        '''if self.player.hit_obj(self.box,0,40,-60,60):
 
             if self.player.x == self.box.x-60:
                 self.player.x -= MOVEMENT_SPEED
@@ -124,15 +198,12 @@ class World:
             elif self.ghost_lv1.y ==self.box.y:
                 self.ghost_lv1.y -= 1
             elif self.ghost_lv1.y == self.box.y+40:
-                self.ghost_lv1.y += 1
+                self.ghost_lv1.y += 1'''
 
-        if self.player.hit_obj(self.ghost_lv1,0,0,-40,40):
-            print("GHOST CAUGHT YOU")
-            
-            if (self.player.x == self.ghost_lv1.x-30 or 
-               self.player.x == self.ghost_lv1.x+30 or
-               self.player.y == self.ghost_lv1.y  ) :
-                quit()
+        
+
+
+                
              
                 
            
--- survive.py
+++ survive.py
@@ -6,6 +6,7 @@ from pyglet.window import key
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+
 class ModelSprite(arcade.Sprite):
     def __init__(self, *args, **kwargs):
         self.model = kwargs.pop('model', None)
@@ -25,44 +26,36 @@ class SurviveWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
   
-        
         self.world = World(width, height)
-         
-        self.player_sprite = ModelSprite("images/player.png",model=self.world.player) #1 self.player_sprite = arcade.Sprite("images/player.png",0.5)
-        self.box_sprite = ModelSprite("images/box.png",model=self.world.box) #self.box_sprite = arcade.Sprite("images/box.png",0.2)
-        self.ghost_sprite = ModelSprite("images/ghost_lv1.png",model=self.world.ghost_lv1)
-
+        
+        self.all_sprites_list = arcade.SpriteList()
+        
+        self.player_sprite = self.world.player
+        
+        self.all_sprites_list.append(self.player_sprite)
+        
+        
         arcade.set_background_color(arcade.color.AMAZON)
 
-    def on_key_press(self, key, modifiers): # checked keyboard : if keyboard pressed - print "key pressed"
-       pass
+    def on_key_press(self, key, modifiers): 
+       self.world.on_key_press(key,modifiers)
+
+    def on_key_release(self, key, modifiers):
+       self.world.on_key_release(key,modifiers)
+    
     
     def on_draw(self):
         arcade.start_render()    
-        self.world.control(keys)
-
-        self.box_sprite.draw() 
+        #self.box_sprite.draw() 
+        self.all_sprites_list.draw()
         
-        if self.world.player.y >= self.world.box.y+40:
-            self.player_sprite.draw()
-            self.box_sprite.draw()
-        else:
-            self.player_sprite.draw()
-
-        if self.world.ghost_lv1.y >= self.world.box.y+40:
-            self.ghost_sprite.draw()
-            self.box_sprite.draw()
-        else:
-            if self.world.player.y < self.world.ghost_lv1.y: 
-                self.ghost_sprite.draw()
-                self.player_sprite.draw()
-            else:
-                self.ghost_sprite.draw()
-
+    
 
     def update(self,delta):
         self.world.update(delta)
-
+        self.all_sprites_list.update()
+        
+        
         
  
 if __name__ == '__main__':
